57a6b18642ca4cfc4a067e4049e33987932c0330
Gabriel Pogor <gabrielpogordima@gmail.com>
2024-04-22 21:44:53 +0200

    RESOLVED: Add CORS policy

--- app/main.py
+++ app/main.py
@@ -1,15 +1,29 @@
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .core import config
 from .routers import auth, incident, reporter
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3080",
+]
+
 app.include_router(auth.router)
 app.include_router(incident.router)
 app.include_router(reporter.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
